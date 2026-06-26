82cf41390849351458ad8951e3c5243aaf961900
JaydenHuang555 <jaydenkanahuang@outlook.com>
2026-06-25 22:22:29 -0700

    core

--- lua/core.lua
+++ lua/core.lua
@@ -4,13 +4,38 @@ local api = vim.api
 local keymap = vim.keymap
 local autocmd = api.nvim_create_autocmd
 
+local buf_set_keymap = api.nvim_buf_set_keymap
 local group = api.nvim_create_augroup("CORE", {clear = true})
+
+-- term
+
+autocmd("TermEnter", {
+	group = group,
+	desc = "bind quick killing",
+	callback = function (args)
+		api.nvim_exec_autocmds("BufEnter", {
+			pattern = "term"
+		})
+	end
+})
+
+
+keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+keymap.set("n", "<Leader>'", "", {
+	noremap = true,
+	silent = true,
+	callback = function ()
+		vim.cmd("sp | term")
+		api.nvim_feedkeys("i", "n", false)
+	end
+})
 
 -- on ft that are quick
 -- add quicker killing binds
 
 function bind_quick_death(bufnr)
-	api.nvim_buf_set_keymap(bufnr, "n", "q", "", {
+	buf_set_keymap(bufnr, "n", "q", "", {
 		silent = true,
 		desc = "quick kill",
 		callback = function ()
@@ -27,41 +52,44 @@ autocmd("BufEnter", {
 	pattern = {
 		"qf",
 		"man",
-		"help"
+		"help",
+		"term"
 	},
 	callback = function (args)
 		bind_quick_death(args.buf)
 	end
 })
 
--- term
 
-autocmd("TermEnter", {
+-- lang
+
+autocmd({"BufReadPre", "BufEnter"}, {
 	group = group,
-	desc = "bind quick killing",
+	pattern = {
+		"c",
+		"cpp",
+		"term",
+		"rust",
+		"rs",
+		"c3",
+		"zig",
+	},
 	callback = function (args)
-		bind_quick_death(args.buf)
+		buf_set_keymap(args.buf, "n", "<Leader>;", "", {
+			desc = "Apply EOS",
+			noremap = true,
+			callback = function ()
+				local row = api.nvim_win_get_cursor(0)[1]
+				local bufnr = api.nvim_get_current_buf()
+				local lines = api.nvim_buf_get_lines(bufnr, row - 1, row - 1, false)
+				if #lines == 0 then
+					api.nvim_buf_set_lines(bufnr, row - 1, row - 1, false, {";"})
+				else
+					local line = lines[1] .. ";"
+					api.nvim_buf_set_lines(bufnr, row - 1, row - 1, false, {line})
+				end
+			end
+		})
 	end
 })
 
-
-keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
-keymap.set("n", "<Leader>;", "", {
-	silent = true,
-	callback = function ()
-		vim.cmd("sp | term")
-		vim.api.nvim_feedkeys("i", "n", false)
-	end
-})
-
-keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
-keymap.set("n", "<Leader>'", "", {
-	noremap = true,
-	silent = true,
-	callback = function ()
-		vim.cmd("sp | term")
-		vim.api.nvim_feedkeys("i", "n", false)
-	end
-})
